017d6fc4ac11eee16e602fc746071654ef055c27
ssipasseuth <sam@taotesting.com>
2014-08-22 16:22:00 +0200

    added qtiItemPci as required

--- manifest.php
+++ manifest.php
@@ -1,5 +1,5 @@
 <?php
-/**  
+/**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -17,46 +17,46 @@
  * Copyright (c) 2014 (original work) Open Assessment Technologies SA;
  *               
  * 
- */               
-
+ */
 return array(
     'name' => 'taoCe',
-	'label' => 'Community Edition',
-	'description' => 'the Community Edition extension',
+    'label' => 'Community Edition',
+    'description' => 'the Community Edition extension',
     'license' => 'GPL-2.0',
     'version' => '1.0.0',
-	'author' => 'Open Assessment Technologies SA',
-	'requires' => array(
-	   'tao' => '>=2.6',
-	   'funcAcl' => '*',
-	   'taoItems' => '>=2.4',
-	       'taoQtiItem' => '*',
-	       'taoOpenWebItem' => '*',
-       'taoTests' => '>=2.4',
-           'taoQtiTest' => '*',
-           'taoWfTest' => '*',
-       'taoSubjects' => '>=2.4',
-       'taoGroups' => '>=2.4',
-       'taoDelivery' => '>=2.4',
-           'taoSimpleDelivery' => '*',
-       'taoResults' => '>=2.4'
+    'author' => 'Open Assessment Technologies SA',
+    'requires' => array(
+        'tao' => '>=2.6',
+        'funcAcl' => '*',
+        'taoItems' => '>=2.4',
+        'taoQtiItem' => '*',
+        'taoOpenWebItem' => '*',
+        'taoTests' => '>=2.4',
+        'taoQtiTest' => '*',
+        'taoWfTest' => '*',
+        'taoSubjects' => '>=2.4',
+        'taoGroups' => '>=2.4',
+        'taoDelivery' => '>=2.4',
+        'taoSimpleDelivery' => '*',
+        'taoResults' => '>=2.4',
+        'qtiItemPci' => '>=0.1'
     ),
-	'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#taoCeManager',
+    'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#taoCeManager',
     'acl' => array(
-        array('grant', 'http://www.tao.lu/Ontologies/TAO.rdf#BackOfficeRole', array('ext'=>'taoCe','mod' => 'Main', 'act' => 'index')),
-        array('grant', 'http://www.tao.lu/Ontologies/TAO.rdf#BackOfficeRole', array('ext'=>'taoCe','mod' => 'Home'))
+        array('grant', 'http://www.tao.lu/Ontologies/TAO.rdf#BackOfficeRole', array('ext' => 'taoCe', 'mod' => 'Main', 'act' => 'index')),
+        array('grant', 'http://www.tao.lu/Ontologies/TAO.rdf#BackOfficeRole', array('ext' => 'taoCe', 'mod' => 'Home'))
     ),
     'routes' => array(
         '/taoCe' => 'oat\\taoCe\\actions'
-    ),    
-	'constants' => array(
-	    # views directory
-	    "DIR_VIEWS" => dirname(__FILE__).DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR,
+    ),
+    'constants' => array(
+        # views directory
+        "DIR_VIEWS" => dirname(__FILE__).DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR,
 	    
-		#BASE URL (usually the domain root)
-		'BASE_URL' => ROOT_URL.'taoCe/',
+        #BASE URL (usually the domain root)
+        'BASE_URL' => ROOT_URL.'taoCe/',
         
         #BASE WWW the web resources path
-		'BASE_WWW'				=> ROOT_URL . 'taoCe/views/'
-	)
+        'BASE_WWW' => ROOT_URL.'taoCe/views/'
+    )
 );
