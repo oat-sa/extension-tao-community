0cf5915700283dd97426e2a9a9ff8ac2ca3119d7
bugalot <jerome@taotesting.com>
2017-10-20 13:51:21 +0200

    Bump to version 3.7.0

--- manifest.php
+++ manifest.php
@@ -23,7 +23,7 @@ return array(
     'label' => 'Community Edition',
     'description' => 'the Community Edition extension',
     'license' => 'GPL-2.0',
-    'version' => '3.6.2',
+    'version' => '3.7.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao' => '>=9.0.0',
