e5b8f223190511c01ce2a317e700ceec283694af
bugalot <jerome@taotesting.com>
2019-07-04 17:56:41 +0200

    Bump version 6.4.0

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -65,6 +65,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('1.8.0');
         }
 
-        $this->skip('1.8.0', '6.3.1');
+        $this->skip('1.8.0', '6.4.0');
     }
 }
