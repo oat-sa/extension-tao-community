a401469a047f3cfeabd93189ff95c492fd81a174
Oleksandr Khitko <oleksandr@taotesting.com>
2020-07-03 11:49:27 +0200

    Version bump.

--- manifest.php
+++ manifest.php
@@ -25,7 +25,7 @@ return [
     'label' => 'Community Edition',
     'description' => 'the Community Edition extension',
     'license' => 'GPL-2.0',
-    'version' => '8.5.0',
+    'version' => '9.0.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => [
         'tao' => '>=36.1.0',
